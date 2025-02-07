6794046a6ab1463760597776388267d5138da422
jquie <jacquelynphan03@gmail.com>
2025-02-07 18:18:20 +0000

    not done

--- Main.java
+++ Main.java
@@ -4,14 +4,34 @@ public class Main
 {
   public static void main(String[] args)
   {
-    // Create some arrays here to test your methods
-    
+    int size;
+    String[] words;
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter array length");
+    size = sc.nextInt();
+    words = new String[size];
+
+    sc.nextLine();
+    System.out.println("Enter" + size + "words");
+    for (int i = 0; i < size; i++)
+    {
+      words[i] = sc.nextLine();
+    }
+    reverse(words);
   }
 
   public static void reverse(String[] arr)
   {
+    for (int n: arr)
+    {
+      String rev = "";
+      for (int i = n.length()-1; i >= 0; i--)
+      {
+        rev += n.substring(i, i+1);
+      }
+      System.out.println(rev);
+    }
 
-  }
 
   public int product(int[] arr)
   {
